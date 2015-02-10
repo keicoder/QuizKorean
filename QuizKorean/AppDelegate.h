1422a20e6569c4c025e6c6357635b654ed1438b9
lovejun <hyun2012@gmail.com>
2015-02-10 15:21:33 +0900

    Initial Commit

---
+++ QuizKorean/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuizKorean
+//
+//  Created by jun on 2/10/15.
+//  Copyright (c) 2015 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuizKorean/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuizKorean
+//
+//  Created by jun on 2/10/15.
+//  Copyright (c) 2015 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
