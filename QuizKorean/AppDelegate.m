e97fd20772c4804b059b142d139887243b5de591
lovejun <hyun2012@gmail.com>
2015-02-15 14:45:02 +0900

    iRate 구현, 애니메이션 조정

--- QuizKorean/AppDelegate.m
+++ QuizKorean/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "iRate.h"
+
 
 @interface AppDelegate ()
 
@@ -14,6 +16,23 @@
 
 @implementation AppDelegate
 
+
+#pragma mark - iRate
+
+//+ (void)initialize
+//{
+//	//set the bundle ID. normally you wouldn't need to do this
+//	//as it is picked up automatically from your Info.plist file
+//	//but we want to test with an app that's actually on the store
+//	[iRate sharedInstance].applicationBundleID = @"Clarity.Clarity";
+//	[iRate sharedInstance].onlyPromptIfLatestVersion = NO;
+//	
+//	//enable preview mode
+//	[iRate sharedInstance].previewMode = YES;
+//}
+
+
+#pragma mark - didFinishLaunchingWithOptions
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
--- QuizKorean/MenuViewController.m
+++ QuizKorean/MenuViewController.m
@@ -28,14 +28,13 @@
 @implementation MenuViewController
 {
 	CGFloat _duration;
-	CGFloat _titleLabelOriginY;
 }
 
 
 - (void)viewDidLoad
 {
 	[super viewDidLoad];
-	_duration = 0.4;
+	_duration = 0.2;
 	[self configureUI];
 }
 
@@ -43,24 +42,73 @@
 - (void)viewWillAppear:(BOOL)animated
 {
 	[super viewWillAppear:animated];
-	
-	CGFloat alpha = 0.0;
-	self.titleLabel.alpha = alpha;
-	self.startButton.alpha = alpha;
-	self.settingsButton.alpha = alpha;
-	self.aboutButton.alpha = alpha;
-	
-	[self.startButton setBackgroundColor: [UIColor colorWithRed:1.000 green:0.541 blue:0.213 alpha:1.000]];
-	
-	[self moveButtonsOutOfTheView];
+	[self changeButtonsAlphaToZero];
+	[self changeTitleLabelsAlphaToOpaque];
 }
 
 
 -(void)viewDidAppear:(BOOL)animated
 {
 	[super viewDidAppear:animated];
+	[self moveButtonsOutOfTheView];
+}
+
+
+#pragma mark - Animation
+
+- (void)moveButtonsOutOfTheView
+{
+	CGFloat originX = 2;
 	
-	[self moveButtonsInTheViewWithAnimation];
+	CGRect startButtonFrame = self.startButton.frame;
+	startButtonFrame.origin.x -= self.view.bounds.size.width/originX;
+	self.startButton.frame = startButtonFrame;
+	
+	CGRect settingsButtonFrame = self.settingsButton.frame;
+	settingsButtonFrame.origin.x -= self.view.bounds.size.width/originX;
+	self.settingsButton.frame = settingsButtonFrame;
+	
+	CGRect aboutButtonFrame = self.aboutButton.frame;
+	aboutButtonFrame.origin.x -= self.view.bounds.size.width/originX;
+	self.aboutButton.frame = aboutButtonFrame;
+	
+	[self performSelector:@selector(moveButtonsInTheViewWithAnimation) withObject:nil afterDelay:0.1];
+}
+
+
+- (void)moveButtonsInTheViewWithAnimation
+{
+	CGFloat alpha = 1.0;
+	
+	CGFloat duration = 0.2;
+	CGFloat initialDelay = 0.0;
+	CGFloat additionalDelay = 0.0;
+	NSUInteger options = UIViewAnimationOptionCurveEaseIn; //from slow to fast
+	
+	[UIView animateWithDuration:duration delay:initialDelay options: options animations:^{
+		
+		self.startButton.alpha = alpha;
+		self.startButton.frame = CGRectMake(self.view.bounds.size.width/2 - self.startButton.bounds.size.width/2, self.startButton.frame.origin.y, self.startButton.bounds.size.width, self.startButton.bounds.size.height);
+		
+	} completion:^(BOOL finished) {
+		
+		[UIView animateWithDuration:duration delay:additionalDelay options: options animations:^{
+			
+			self.settingsButton.alpha = alpha;
+			self.settingsButton.frame = CGRectMake(self.view.bounds.size.width/2 - self.settingsButton.bounds.size.width/2, self.settingsButton.frame.origin.y, self.settingsButton.bounds.size.width, self.settingsButton.bounds.size.height);
+			
+		} completion:^(BOOL finished) {
+			
+			[UIView animateWithDuration:duration delay:additionalDelay options: options animations:^{
+				
+				self.aboutButton.alpha = alpha;
+				self.aboutButton.frame = CGRectMake(self.view.bounds.size.width/2 - self.aboutButton.bounds.size.width/2, self.aboutButton.frame.origin.y, self.aboutButton.bounds.size.width, self.aboutButton.bounds.size.height);
+				
+			} completion:^(BOOL finished) {
+				
+			}];
+		}];
+	}];
 }
 
 
@@ -107,95 +155,6 @@
 }
 
 
-#pragma mark - Animation
-
-- (void)moveButtonsOutOfTheView
-{
-	CGRect titleLabelFrame = self.titleLabel.frame;
-	titleLabelFrame.origin.y -= self.view.bounds.size.height/2;
-	self.titleLabel.frame = titleLabelFrame;
-	
-	CGRect startButtonFrame = self.startButton.frame;
-	startButtonFrame.origin.x -= self.view.bounds.size.width/2;
-	self.startButton.frame = startButtonFrame;
-	
-	CGRect settingsButtonFrame = self.settingsButton.frame;
-	settingsButtonFrame.origin.x -= self.view.bounds.size.width/2;
-	self.settingsButton.frame = settingsButtonFrame;
-	
-	CGRect aboutButtonFrame = self.aboutButton.frame;
-	aboutButtonFrame.origin.x -= self.view.bounds.size.width/2;
-	self.aboutButton.frame = aboutButtonFrame;
-}
-
-
-- (void)moveButtonsInTheViewWithAnimation
-{
-	CGFloat alpha = 1.0;
-	
-	CGFloat duration = 0.15;
-	CGFloat initialDelay = 0.1;
-	CGFloat additionalDelay = 0.0;
-	NSUInteger options = UIViewAnimationOptionCurveEaseIn; //from slow to fast
-	
-	[UIView animateWithDuration:duration delay:initialDelay options: options animations:^{
-		
-		self.startButton.alpha = alpha;
-		self.startButton.frame = CGRectMake(self.view.bounds.size.width/2 - self.startButton.bounds.size.width/2, self.startButton.frame.origin.y, self.startButton.bounds.size.width, self.startButton.bounds.size.height);
-		
-	} completion:^(BOOL finished) {
-		
-		[UIView animateWithDuration:duration delay:additionalDelay options: options animations:^{
-			
-			self.settingsButton.alpha = alpha;
-			self.settingsButton.frame = CGRectMake(self.view.bounds.size.width/2 - self.settingsButton.bounds.size.width/2, self.settingsButton.frame.origin.y, self.settingsButton.bounds.size.width, self.settingsButton.bounds.size.height);
-			
-		} completion:^(BOOL finished) {
-			
-			[UIView animateWithDuration:duration delay:additionalDelay options: options animations:^{
-				
-				self.aboutButton.alpha = alpha;
-				self.aboutButton.frame = CGRectMake(self.view.bounds.size.width/2 - self.aboutButton.bounds.size.width/2, self.aboutButton.frame.origin.y, self.aboutButton.bounds.size.width, self.aboutButton.bounds.size.height);
-				
-			} completion:^(BOOL finished) {
-				
-				[UIView animateWithDuration:duration delay:additionalDelay options: options animations:^{
-					
-					self.titleLabel.alpha = alpha;
-					self.titleLabel.frame = CGRectMake(self.view.bounds.size.width/2 - self.titleLabel.bounds.size.width/2, _titleLabelOriginY, self.titleLabel.bounds.size.width, self.titleLabel.bounds.size.height);
-					
-				} completion:^(BOOL finished) {
-					
-					CGFloat labelAnimationDuration = 0.2;
-					
-					[UIView animateWithDuration:labelAnimationDuration delay:additionalDelay options: UIViewAnimationOptionCurveEaseInOut animations:^{
-						
-						self.titleLabel.transform = CGAffineTransformMakeScale(1.1, 1.1);
-						
-					} completion:^(BOOL finished) {
-						
-						[UIView animateWithDuration:labelAnimationDuration animations:^{
-							
-							self.titleLabel.transform = CGAffineTransformMakeScale(0.9, 0.9);
-							
-						}completion:^(BOOL finished) {
-							
-							[UIView animateWithDuration:labelAnimationDuration animations:^{
-								
-								self.titleLabel.transform = CGAffineTransformMakeScale(1.0, 1.0);
-								
-							}completion:^(BOOL finished) {
-								
-							}];
-						}];
-					}];
-				}];
-			}];
-		}];
-	}];
-}
-
-
 #pragma mark - Configure UI
 
 - (void)configureUI
@@ -204,8 +163,30 @@
 	self.startButton.layer.cornerRadius = cornerRadius;
 	self.settingsButton.layer.cornerRadius = cornerRadius;
 	self.aboutButton.layer.cornerRadius = cornerRadius;
+}
+
+
+- (void)changeButtonsAlphaToZero
+{
+	CGFloat alpha = 0.0;
+	self.titleLabel.alpha = alpha;
+	self.startButton.alpha = alpha;
+	self.settingsButton.alpha = alpha;
+	self.aboutButton.alpha = alpha;
 	
-	_titleLabelOriginY = self.titleLabel.frame.origin.y;
+	[self.startButton setBackgroundColor: [UIColor colorWithRed:1.000 green:0.541 blue:0.213 alpha:1.000]];
+}
+
+
+- (void)changeTitleLabelsAlphaToOpaque
+{
+	CGFloat labelAnimationDuration = 0.7;
+	[UIView animateWithDuration:labelAnimationDuration delay:0.0 options: UIViewAnimationOptionCurveEaseInOut animations:^{
+		
+		CGFloat alpha = 1.0;
+		self.titleLabel.alpha = alpha;
+		
+	} completion:^(BOOL finished) { }];
 }
 
 
